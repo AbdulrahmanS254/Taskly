b6a9e7554ddbcefed9e5cf2b37eff1051e0a717e
Abdulrahman Shawky <hawk18922@gmail.com>
2026-07-21 14:39:58 +0300

    fix(auth & routing): forgot to add the protected route to routes/index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import LoginPage from '../features/auth/pages/LoginPage';
 import SignUpPage from '../features/auth/pages/SignUpPage';
 import DashboardLayout from '../components/layout/DashboardLayout';
 import GuestRoute from '../components/layout/GuestRoute';
+import ProtectedRoute from './ProtectedRoute';
 
 export const router = createBrowserRouter([
     {
@@ -23,31 +24,36 @@ export const router = createBrowserRouter([
         ],
     },
     {
-        element: <DashboardLayout />,
+        element: <ProtectedRoute />,
         children: [
             {
-                path: '/project',
-                element: <Navigate to="/projects" replace />,
-            },
-            {
-                path: '/projects',
-                element: <div>Projects Page Content</div>,
-            },
-            {
-                path: '/epics',
-                element: <div>Project Epics Content</div>,
-            },
-            {
-                path: '/tasks',
-                element: <div>Project Tasks Content</div>,
-            },
-            {
-                path: '/members',
-                element: <div>Project Members Content</div>,
-            },
-            {
-                path: '/details',
-                element: <div>Project Details Content</div>,
+                element: <DashboardLayout />,
+                children: [
+                    {
+                        path: '/project',
+                        element: <Navigate to="/projects" replace />,
+                    },
+                    {
+                        path: '/projects',
+                        element: <div>Projects Page Content</div>,
+                    },
+                    {
+                        path: '/epics',
+                        element: <div>Project Epics Content</div>,
+                    },
+                    {
+                        path: '/tasks',
+                        element: <div>Project Tasks Content</div>,
+                    },
+                    {
+                        path: '/members',
+                        element: <div>Project Members Content</div>,
+                    },
+                    {
+                        path: '/details',
+                        element: <div>Project Details Content</div>,
+                    },
+                ],
             },
         ],
     },
